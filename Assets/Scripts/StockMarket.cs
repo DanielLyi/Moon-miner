31a65bdf8fbb089e070193e91030855a6ea4e2de
Danylo <danylo.liakh@gmail.com>
2021-07-17 10:14:11 +0300

    Started creating the trading screen

--- Assets/Scripts/StockMarket.cs
+++ Assets/Scripts/StockMarket.cs
@@ -8,8 +8,9 @@ public class StockMarket : MonoBehaviour
 {
     private Random _random = new Random();
     
-    private double _price = 2000.5f;
-    
+    private float _bestAskPrice = 2000.6f;
+    private float _bestBidPrice = 2000.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,19 +21,20 @@ public class StockMarket : MonoBehaviour
     {
         while (true)
         {
-            double u1 = 1.0-_random.NextDouble(); //uniform(0,1] random doubles
-            double u2 = 1.0-_random.NextDouble();
-            double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) *
-                                   Math.Sin(2.0 * Math.PI * u2); //random normal(0,1)
-            _price = _price + 10 * randStdNormal; //random normal(mean,stdDev^2)
-            Debug.Log(_price.ToString());
+            _bestBidPrice = _bestBidPrice + 10 * GetNormalGaussRandom(); //random normal(mean,stdDev^2)
+            _bestAskPrice = _bestBidPrice + 1 * Mathf.Abs(GetNormalGaussRandom());
+            Debug.Log("bid " + _bestBidPrice.ToString());
+            Debug.Log("ask " + _bestAskPrice.ToString());
             yield return new WaitForSeconds(0.2f);
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private float GetNormalGaussRandom()
     {
-        
+        double u1 = 1.0-_random.NextDouble(); //uniform(0,1] random doubles
+        double u2 = 1.0-_random.NextDouble();
+        double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) *
+                               Math.Sin(2.0 * Math.PI * u2); //random normal(0,1)
+        return (float) randStdNormal;
     }
 }
